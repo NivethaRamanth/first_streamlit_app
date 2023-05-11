41e408bd9f063da99b19caa42d73e2305075f99c
Nivetha Ramnath <36983003+NivethaRamanth@users.noreply.github.com>
2023-05-11 11:23:07 -0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -5,7 +5,7 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Rang Egg')
-streamlit.header('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
