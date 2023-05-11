79fdcf31b6555d763412bb19dc977aec0492fc7b
Nivetha Ramnath <36983003+NivethaRamanth@users.noreply.github.com>
2023-05-11 11:20:36 -0700

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,2 +1,10 @@
 import streamlit
-stream.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Rang Egg')
+
+
+
